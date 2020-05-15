328668e451f03e5839f06a767bc31a0b23d97f74
Thomas Lohse <thomas.k.lohse@gmail.com>
2020-05-15 13:18:04 +0200

    move

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float speed = 10;
+
+    public Transform me;
+    float rotationH = 5;
+    float rotationV = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translationZ = Input.GetAxis("Vertical") * speed;
+        float translationX = Input.GetAxis("Horizontal") * speed;
+        translationZ *= Time.deltaTime;
+        translationX *= Time.deltaTime;
+        transform.Translate(translationX, 0, translationZ);
+
+        float h = Input.GetAxis("Mouse X") * rotationH;
+        float v = Input.GetAxis("Mouse Y") * rotationV;
+      
+        Vector3 rot = new Vector3(v, h, 0);
+        transform.Rotate(rot, Space.Self);
+
+    }
+}
